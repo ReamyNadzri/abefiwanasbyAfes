1d857cce825a9f72d4472ec141fa6f5b652463e3
MUHAMMAD HAZIQ AKRAM BIN MOHD ASHRI <2024741533@student.uitm.edu.my>
2025-01-05 23:04:33 +0800

    Update car_info.php

--- admin/car_info.php
+++ admin/car_info.php
@@ -1,37 +1,38 @@
-
 <?PHP
 # Memanggil fail header_admin.php
-include ('header_admin.php');
+include('header_admin.php');
 # Memanggil fail connection dari folder luaran
-include ('../connection.php');
+include('../connection.php');
 
 #----------- Bahagian 2 : Proses penyimpan data-------
 # Menyemak kewujudan data POST
-if(!empty($_POST))
-{
+if (!empty($_POST)) {
     # mengambil data POST
-    $numPlate=$_POST['numPlate'];
-    $carName=$_POST['carName'];
-    $carType=$_POST['carType'];
-    $model_ID=$_POST['model_ID'];
-    $color=$_POST['color'];
-    $yearManufac=$_POST['yearManufac'];
-    $initialPrice=$_POST['initialPrice'];
-    $desccar=$_POST['desccar'];
-    $idimg=$_POST['idimg'];
+    $numPlate = $_POST['numPlate'];
+    $carName = $_POST['carName'];
+    $carType = $_POST['carType'];
+    $color = $_POST['color'];
+    $yearManufac = $_POST['yearManufac'];
+    $initialPrice = $_POST['initialPrice'];
+    $desccar = $_POST['desccar'];
+    $transmission = $_POST['transmission'];
+    $odometer = $_POST['odometer'];
+    $variant = $_POST['variant'];
+    $fuelType = $_POST['fuelType'];
+    $seat = $_POST['seat'];
+    $cc = $_POST['cc'];
+    $model_ID = $_POST['model_ID'];
+    $idimg = $_POST['idimg'];
     # Arahan untuk menyimpan data ke dalam jadual kereta
-    $arahan_sql_simpan="insert into car
+    $arahan_sql_simpan = "insert into car
     values
     ('$numPlate','$carName','$carType','$color','$yearManufac','$initialPrice','$desccar','$model_ID','$idimg')";
 
     # melaksanakan proses menyimpan dalam syarat IF
-    if(mysqli_query($condb, $arahan_sql_simpan))
-    {
+    if (mysqli_query($condb, $arahan_sql_simpan)) {
         # proses menyimpan data berjaya. papar mesej
         echo "<script>alert('Adding new data was successful');</script>";
-    }
-    else
-    {
+    } else {
         # proses menyimpan data gagal. papar mesej
         echo "<script>alert('Registration Failure');
         window.history.back();</script>";
@@ -43,14 +44,14 @@ if(!empty($_POST))
 # ----------- bahagian 1 : memaparkan data dalam bentuk jadual
 
 # arahan SQL mencari kereta yang masih belum dijual
-$arahan_sql_cari="select* from car,model where car.numPlate not in (select numPlate from purchase) and car.model_ID=model.model_ID order by idimg desc";
+$arahan_sql_cari = "select* from car,model where car.numPlate not in (select numPlate from purchase) and car.model_ID=model.model_ID order by idimg desc";
 # melaksanakan arahan sql cari tersebut
-$laksana_sql_cari=mysqli_query($condb, $arahan_sql_cari);
+$laksana_sql_cari = mysqli_query($condb, $arahan_sql_cari);
 ?>
 
 <style>
-    .adjust{
-        width:1000px
+    .adjust {
+        width: 1000px
     }
 </style>
 
@@ -63,8 +64,8 @@ $laksana_sql_cari=mysqli_query($condb, $arahan_sql_cari);
 <table class="w3-table-all" id='saiz' border='1'>
     <tr class="w3-light-blue">
         <td>Bil</td>
-        <td  id="adjust">Plate Number</td>
-        
+        <td id="adjust">Plate Number</td>
+
         <td>Car Name</td>
         <td>Car Type</td>
         <td>Model</td>
@@ -87,19 +88,18 @@ $laksana_sql_cari=mysqli_query($condb, $arahan_sql_cari);
                 <select name='model_ID' required>
                     <option disabled selected value>Category</option>
                     <?PHP
-                
-                # arahan mencari data dari jadual model 
-                $arahan_sql_carimodel= "SELECT* from model";
-                # melaksanakan arahan mencari tersebut
-                $laksana_sql_carimodel=mysqli_query($condb,$arahan_sql_carimodel);
-                # pembolehubah $rekod_model mengambil data baris demi baris 
-                while($rekod_model=mysqli_fetch_array($laksana_sql_carimodel))
-                {
-                    # memaparkan nilai pemboleh ubah $rekod_model['modelName'] dalam bentuk dropdown list
-                    echo"<option value='".$rekod_model['model_ID']."'>
-                    ".$rekod_model['modelName']."</option>";        
-                }
-                ?>
+
+                    # arahan mencari data dari jadual model 
+                    $arahan_sql_carimodel = "SELECT* from model";
+                    # melaksanakan arahan mencari tersebut
+                    $laksana_sql_carimodel = mysqli_query($condb, $arahan_sql_carimodel);
+                    # pembolehubah $rekod_model mengambil data baris demi baris 
+                    while ($rekod_model = mysqli_fetch_array($laksana_sql_carimodel)) {
+                        # memaparkan nilai pemboleh ubah $rekod_model['modelName'] dalam bentuk dropdown list
+                        echo "<option value='" . $rekod_model['model_ID'] . "'>
+                    " . $rekod_model['modelName'] . "</option>";
+                    }
+                    ?>
                 </select>
             </td>
             <td><input type='text' name='color'></td>
@@ -108,42 +108,48 @@ $laksana_sql_cari=mysqli_query($condb, $arahan_sql_cari);
             <td><input type='text' name='desccar' style="width: 200px;height:100px"></td>
             <td>
                 <!----------------------------------------------------------------------------------------------------------------------------------------------->
-            
-                    <?PHP
-                    if(empty($_GET['idimg'])){
+
+                <?PHP
+                if (empty($_GET['idimg'])) {
                     $idimg = "Not Selected";
-                    }else{
+                } else {
                     $idimg = $_GET['idimg'];
                     $rekod['idimg'] = $idimg;
-                    }
-                    ?>
-                    <input text='text' name='idimg' size="3" value='<?php echo"$idimg"; ?>'></td>
-                
-                <!----------------------------------------------------------------------------------------------------------------------------------------------->
+                }
+                ?>
+                <input text='text' name='idimg' size="3" value='<?php echo "$idimg"; ?>'>
+            </td>
+
+            <!----------------------------------------------------------------------------------------------------------------------------------------------->
             <td><input type='submit' value='Save'></td>
         </form>
     </tr>
-    <?PHP 
+    <?PHP
 
-    $bil=0;
+    $bil = 0;
     # pemboleh ubah $rekod mengambail semua data yang ditemui oleh $laksana_sql_cari
-    while ($rekod=mysqli_fetch_array($laksana_sql_cari)) 
-    {
+    while ($rekod = mysqli_fetch_array($laksana_sql_cari)) {
         # sistem akan memaparkan data $rekod baris demi baris sehingga habis
         echo "
         <tr>
-            <td>".++$bil."</td>
-            <td>".$rekod['numPlate']."</td>
-            <td>".$rekod['carName']."</td>
-            <td>".$rekod['carType']."</td>
-            <td>".$rekod['modelName']."</td>
-            <td>".$rekod['color']."</td>
-            <td>".$rekod['yearManufac']."</td>
-            <td>".$rekod['initialPrice']."</td>
-            <td>".$rekod['desccar']."</td>
-            <td>".$rekod['idimg']."</td>
+            <td>" . ++$bil . "</td>
+            <td>" . $rekod['numPlate'] . "</td>
+            <td>" . $rekod['carName'] . "</td>
+            <td>" . $rekod['carType'] . "</td>
+            <td>" . $rekod['modelName'] . "</td>
+            <td>" . $rekod['color'] . "</td>
+            <td>" . $rekod['yearManufac'] . "</td>
+            <td>" . $rekod['initialPrice'] . "</td>
+            <td>" . $rekod['desccar'] . "</td>
+            <td>" . $_POST['transmission'] . "</td>
+            <td>" . $_POST['odometer'] . "<td>
+            <td>" . $_POST['variant'] . "</td>
+            <td>" . $_POST['fuelType'] . "</td>
+            <td>" . $_POST['seat'] . "</td>
+            <td>" . $_POST['cc'] . "</td>
+            <td>" . $rekod['idimg'] . "</td>
 
-            <td><a href='hapus.php?jadual=car&medan_kp=numPlate&kp=".$rekod['numPlate']."' onClick=\"return confirm('Confirm to delete data?')\" >Delete</a></td>
+            <td><a href='hapus.php?jadual=car&medan_kp=numPlate&kp=" . $rekod['numPlate'] . "' onClick=\"return confirm('Confirm to delete data?')\" >Delete</a></td>
         </tr>";
     }
     ?>
