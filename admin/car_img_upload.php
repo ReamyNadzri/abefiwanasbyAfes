1a084d82ae8a9096301bc06efcb7a189aea6ae70
Ikhwan Hadi <ikhwanhadi32@gmail.com>
2025-01-06 22:45:48 +0800

    car img fixed
    gg

--- admin/car_img_upload.php
+++ admin/car_img_upload.php
@@ -33,7 +33,8 @@ include('../connection.php');
         <input class="pad" type="file" name="sideimage3">
 
     </div><br><br>
-    <input class="w3-center w3-button w3-round-xlarge" size="15" style="background: #FFBF00; " type="submit" name="submit" value="Upload Files"><br><br><br>
+    <input class="w3-center w3-button w3-round-xlarge" size="15" style="background: #FFBF00; " type="submit"
+        name="submit" value="Upload Files"><br><br><br>
 </form>
 
 <?php
@@ -65,40 +66,56 @@ if (isset($_POST["submit"])) {
 
         if (in_array($fileType0, $allowTypes) && in_array($fileType1, $allowTypes) && in_array($fileType2, $allowTypes) && in_array($fileType3, $allowTypes)) {
 
-            $image0 = $_FILES['image']['tmp_name'];
-            $imgContent0 = addslashes(file_get_contents($image0));
+            // Initialize BLOB descriptors
+            $lob0 = oci_new_descriptor($condb, OCI_D_LOB);
+            $lob1 = oci_new_descriptor($condb, OCI_D_LOB);
+            $lob2 = oci_new_descriptor($condb, OCI_D_LOB);
+            $lob3 = oci_new_descriptor($condb, OCI_D_LOB);
 
-            $image1 = $_FILES['sideimage1']['tmp_name'];
-            $imgContent1 = addslashes(file_get_contents($image1));
+            // Read image files
+            $image0 = file_get_contents($_FILES['image']['tmp_name']);
+            $image1 = file_get_contents($_FILES['sideimage1']['tmp_name']);
+            $image2 = file_get_contents($_FILES['sideimage2']['tmp_name']);
+            $image3 = file_get_contents($_FILES['sideimage3']['tmp_name']);
 
-            $image2 = $_FILES['sideimage2']['tmp_name'];
-            $imgContent2 = addslashes(file_get_contents($image2));
-
-            $image3 = $_FILES['sideimage3']['tmp_name'];
-            $imgContent3 = addslashes(file_get_contents($image3));
-
-            // Insert image content into database 
+            // SQL for Oracle BLOB insert
             $arahan_sql_simpan = "
-                INSERT INTO IMAGES (image, sideimages1, sideimages2, sideimages3, datecreate)
-                VALUES (:image, :sideimages1, :sideimages2, :sideimages3, SYSDATE)
-            ";
+    INSERT INTO IMAGES (image, sideimages1, sideimages2, sideimages3, datecreate)
+    VALUES (EMPTY_BLOB(), EMPTY_BLOB(), EMPTY_BLOB(), EMPTY_BLOB(), SYSDATE)
+    RETURNING image, sideimages1, sideimages2, sideimages3 INTO :image, :sideimages1, :sideimages2, :sideimages3
+";
 
-            # Melaksanakan proses menyimpan
-            $laksana_arahan = oci_parse($condb, $arahan_sql_simpan);
+            // Parse and bind BLOBs
+            $stmt = oci_parse($condb, $arahan_sql_simpan);
+            oci_bind_by_name($stmt, ":image", $lob0, -1, SQLT_BLOB);
+            oci_bind_by_name($stmt, ":sideimages1", $lob1, -1, SQLT_BLOB);
+            oci_bind_by_name($stmt, ":sideimages2", $lob2, -1, SQLT_BLOB);
+            oci_bind_by_name($stmt, ":sideimages3", $lob3, -1, SQLT_BLOB);
 
-            # Bind parameter untuk mengelakkan SQL Injection
-            oci_bind_by_name($laksana_arahan, ':image', $imgContent0);
-            oci_bind_by_name($laksana_arahan, ':sideimages1', $imgContent1);
-            oci_bind_by_name($laksana_arahan, ':sideimages2', $imgContent2);
-            oci_bind_by_name($laksana_arahan, ':sideimages3', $imgContent3);
+            if (oci_execute($stmt, OCI_DEFAULT)) {
+                // Write BLOB data
+                $lob0->save($image0);
+                $lob1->save($image1);
+                $lob2->save($image2);
+                $lob3->save($image3);
 
-            if (oci_execute($laksana_arahan, OCI_COMMIT_ON_SUCCESS)) {
-                oci_free_statement($laksana_arahan);
+                // Commit transaction
+                oci_commit($condb);
+
+                // Free resources
+                $lob0->free();
+                $lob1->free();
+                $lob2->free();
+                $lob3->free();
+                oci_free_statement($stmt);
                 oci_close($condb);
+
                 $status = 'success';
                 $statusMsg = "File uploaded successfully.";
             } else {
-                $statusMsg = "File upload failed, please try again.";
+                $e = oci_error($stmt);
+                $status = 'error';
+                $statusMsg = "Upload failed: " . htmlentities($e['message']);
             }
         } else {
             $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
