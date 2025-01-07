0ea5067a06ad991efdacf2d0a2fb75f4cf0598a0
MUHAMMAD HAZIQ AKRAM BIN MOHD ASHRI <2024741533@student.uitm.edu.my>
2025-01-07 15:16:41 +0800

    Update purchase_listing.php

--- purchase_listing.php
+++ purchase_listing.php
@@ -1,40 +1,41 @@
-<?php include ('header.php'); 
+<?php include('header.php');
 
 ?><br>
 
 <div class="w3-container w3-row w3-center w3-round-large" style="margin:0 auto;width:60%;"><br>
-    <h1 >Find your favorites car here! Total Finding</h1>
+    <h1>Find your favorites car here! Total Finding</h1>
     <div class="w3-col w3-padding w3-container w3-card w3-round-large" style=" margin-bottom:10px">
-        
+
         <form class="" method="GET" action='purchase_listing.php'>
-        
+
             <input class="w3-col w3-input w3-round" style="width:90%" type="text" name="search"
-            placeholder="What car are you looking for?">
-            
+                placeholder="What car are you looking for?">
+
             <div class="w3-col" style="width:10%">
-                <button class="w3-button w3-round" ><i class="material-icons"  name='submit'>search</i></a>
+                <button class="w3-button w3-round"><i class="material-icons" name='submit'>search</i></a>
             </div>
 
-           
+
         </form>
-        
+
     </div>
     <br><br>
 
-<?php
- 
-  if(!empty($_GET['search'])){
-    
-                # Memanggil fail connection
-            include('connection.php');
+    <?php
 
-            # Mengambil data GET
-            $model = $_GET['search'];
+    if (!empty($_GET['search'])) {
 
-            ########################################################################################################################################
+        # Memanggil fail connection
+        include('connection.php');
 
-            # Arahan mencari data dari jadual car yang tidak wujud di jadual purchase
-            $arahan_sql_cari = "
+        # Mengambil data GET
+        /* $model = $_GET['search']; */
+        $model = '%' . strtoupper($_GET['search']) . '%';
+        
+        ########################################################################################################################################
+
+        # Arahan mencari data dari jadual car yang tidak wujud di jadual purchase
+        /* $arahan_sql_cari = "
                 SELECT car.*, model.modelName, images.image 
                 FROM car, model, images 
                 WHERE car.numPlate NOT IN (SELECT numPlate FROM purchase)
@@ -42,47 +43,72 @@
                 AND car.idimg = images.idimg
                 AND (car.carName LIKE '%' || :SEARCH || '%' 
                     OR model.modelName LIKE '%' || :SEARCH || '%' 
-                    OR car.yearManufac LIKE '%' || :SEARCH || '%')";
+                    OR car.yearManufac LIKE '%' || :SEARCH || '%')"; */
 
-            # Melaksanakan proses carian
-            $laksana_arahan = oci_parse($condb, $arahan_sql_cari);
+        $arahan_sql_cari =  "
+                SELECT 
+                    CAR.*, 
+                    MODEL.MODELNAME,
+                    IMAGES.IDIMG
+                FROM CAR
+                INNER JOIN MODEL ON CAR.MODEL_ID = MODEL.MODEL_ID
+                LEFT OUTER JOIN IMAGES ON CAR.IDIMG = IMAGES.IDIMG
+                WHERE CAR.NUMPLATE NOT IN (SELECT NUMPLATE FROM PURCHASE)
+                AND (
+                    UPPER(CAR.CARNAME) LIKE UPPER(:search)
+                    OR UPPER(MODEL.MODELNAME) LIKE UPPER(:search)
+                )
+            
+        ";
 
-            # Bind parameter untuk carian
-            oci_bind_by_name($laksana_arahan, ':SEARCH', $model);
+        # Melaksanakan proses carian
+        $laksana_arahan = oci_parse($condb, $arahan_sql_cari);
 
-            # Laksanakan arahan SQL
-            oci_execute($laksana_arahan);
+        # Bind parameter untuk carian
+        oci_bind_by_name($laksana_arahan, ':SEARCH', $model);
 
-            # Jika terdapat data yang ditemui
-            if (oci_fetch_all($laksana_arahan, $rekod, null, null, OCI_FETCHSTATEMENT_BY_ROW) > 0) {
-                # Papar header kepada jadual
-                echo "<i style='font-size: small;'> Showing " . count($rekod) . " result(s) of \" " . htmlspecialchars($model) . " \".</i><br><br>";
+        # Laksanakan arahan SQL
+        oci_execute($laksana_arahan);
 
-                # Memaparkan data yang ditemui semasa proses carian baris demi baris
-                foreach ($rekod as $row) {
-                    # Mengumpulkan nilai tatasusunan untuk data GET
-                    $data_get = array(
-                        'numPlate' => $row['NUMPLATE'],
-                        'carName' => $row['CARNAME'],
-                        'carType' => $row['CARTYPE'],
-                        'color' => $row['COLOR'],
-                        'yearManufac' => $row['YEARMANUFAC'],
-                        'desccar' => $row['DESCCAR'],
-                        'initialPrice' => $row['INITIALPRICE'],
-                        'modelName' => $row['MODELNAME'],
-                    );
+        # Jika terdapat data yang ditemui
+        if (oci_fetch_all($laksana_arahan, $rekod, null, null, OCI_FETCHSTATEMENT_BY_ROW) > 0) {
+            # Papar header kepada jadual
+            echo "<i style='font-size: small;'> Showing " . count($rekod) . " result(s) of \" " . htmlspecialchars($model) . " \".</i><br><br>";
 
-                    # Memaparkan maklumat kenderaan
-                    echo "
+            # Memaparkan data yang ditemui semasa proses carian baris demi baris
+            foreach ($rekod as $row) {
+                # Mengumpulkan nilai tatasusunan untuk data GET
+                $data_get = array(
+                    'numPlate' => $row['NUMPLATE'],
+                    'carName' => $row['CARNAME'],
+                    'carType' => $row['CARTYPE'],
+                    'color' => $row['COLOR'],
+                    'yearManufac' => $row['YEARMANUFAC'],
+                    'desccar' => $row['DESCCAR'],
+                    'initialPrice' => $row['INITIALPRICE'],
+                    'modelName' => $row['MODELNAME'],
+                );
+
+                echo $row['NUMPLATE']; 
+                echo $row['CARNAME']; 
+                echo $row['CARTYPE']; 
+                echo $row['COLOR']; 
+                echo $row['YEARMANUFAC']; 
+                echo $row['DESCCAR']; 
+                echo $row['INITIALPRICE']; 
+                echo $row['MODELNAME']; 
+
+                # Memaparkan maklumat kenderaan
+                echo "
                     <a href=purchase_view.php?" . http_build_query($data_get) . ">
                     <div class='w3-col w3-container w3-card w3-round-large' style='margin-bottom:15px;text-align:left; width: 100%; height: 200px;'>
                         <div class='w3-quarter w3-border w3-center' style='height: 100%;'>";
-                    ?>
+    ?>
 
-                    <img class="mw396 w3-center" data-testid="cover-image" style="height:180px;width:auto ;padding-top:10px" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['IMAGE']); ?>" />
 
-                    <?php
-                    echo "
+
+    <?php
+                echo "
                         </div>
                         <div class='w3-threequarter' style='height:60px;padding-left:10px; padding-bottom:10px'>
                             <h3><b>" . htmlspecialchars($row['CARNAME']) . "</b></h3>
@@ -109,16 +135,16 @@
                             </div>
                         </div>
                     </div></a>";
-                }
-            } else {
-                # Jika tiada data ditemui
-                echo "<i><br><br>The car you are looking for may not be in stock or has been sold out.<br>You can continue searching next :)<br><br></i>";
             }
+        } else {
+            # Jika tiada data ditemui
+            echo "<i><br><br>The car you are looking for may not be in stock or has been sold out.<br>You can continue searching next :)<br><br></i>";
         }
+    }
 
-  ?>
+    ?>
 
 </div>
 <br>
 
-<?PHP include ('footer.php'); ?>
+<?PHP include('footer.php'); ?>
